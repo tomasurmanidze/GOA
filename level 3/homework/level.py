e68b4243b95640f874b73a2d164b3d17283e7744
tomasurmanidze <surmanidzet471@gmail.com>
2024-06-13 18:38:33 +0400

    second commit

---
+++ level 3/homework/level.py
@@ -0,0 +1,11 @@
+name = "toma"
+surename = "surmanidze"
+age = 12 #ცვლადს დავარქვი age და მას მნიშვნელობა მივანიჭე 12 ანუ ჩემი ასაკი
+weight = 62.8
+height = 1.70
+hobby = "ride a bike"
+favorite_color = "every color"
+favorite_car = "smart"
+city = "batumi"
+favorite_food = "sushi"
+print(name,surename,age,weight,city,favorite_color,favorite_car,favorite_food,hobby)
