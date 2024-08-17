242841b494c1b60e24d697a1837abfa38e73a122
tomasurmanidze <surmanidzet471@gmail.com>
2024-08-17 18:07:52 +0400

    goa commit

---
+++ level 08/cw/level8.py
@@ -0,0 +1,6 @@
+name = "თომა სურმანიდძე"
+hobby = "პროგრამირება"
+favorite_academi = "GOA"
+favorite_drink = "წყალი"
+favorite_programing_language = "phyton html lua"
+print(name,hobby,favorite_academi,favorite_drink,favorite_programing_language)
