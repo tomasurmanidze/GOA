b69f47b13cc44bc10866c58fc398c5a32d45bfaf
tomasurmanidze <surmanidzet471@gmail.com>
2024-09-09 15:30:30 +0400

    goa commit

---
+++ level 12/hw/level 12.py
@@ -0,0 +1,53 @@
+variable_1 = int(input("please enter 1 variable:"))
+print(type(variable_1))
+
+variable_2 = input("please enter 2 variable:")
+print(type(variable_2))
+
+variable_3 = float(input("please enter 3 variable:"))
+print(type(variable_3))
+
+variable_4 = int(input("please enter 4 variable:"))
+print(type(variable_4))
+
+variable_5 = input("please enter 5 variable:")
+print(type(variable_5))
+
+variable_6 = input("please enter 6 variable:")
+print(type(variable_6))
+
+variable_7 = input("please enter 7 variable:")
+print(type(variable_7))
+
+variable_8 = input("please enter 8 variable:")
+print(type(variable_8))
+
+variable_9 = input("please enter 9 variable:")
+print(type(variable_9))
+
+variable_10 = input("please enter 10 variable:")
+print(type(variable_10))
+
+
+
+
+variable_11 = int(input("please enter 1 variable:"))
+print(type(variable_1))
+
+variable_12 = input("please enter 2 variable:")
+print(type(variable_2))
+
+variable_13 = float(input("please enter 3 variable:"))
+print(type(variable_3))
+
+
+age = str('77')
+print(type(age)) 
+
+
+random = float('45.67')
+print(type(random))
+
+
+random_2 = str(45)
+print(type(random_2))
